9a8ec8a76aa6e1df1f6256d12d0cde0c87fe14a2
kishorenc <kishorenc@gmail.com>
2018-03-05 07:33:48 +0530

    Created a publish script for release.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Typesense Python Client
+
+Python client for the Typesense API: https://github.com/typesense/typesense
+
+Follows the Typesense [API specification](https://github.com/typesense/typesense-api-spec).
+
+## Installation
+
+```
+$ pip install typesense
+```
+
+You can also add `typesense` to your project's `requirements.txt`.
+
+## Usage
+
+See the detailed [API documentation](https://typesense.org/api).
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at [https://github.com/typesense/typesense-python].
+
+## License
+
+`typesense` is distributed under the Apache 2 license.
+
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
 
+from m2r import parse_from_file
+
+long_description = parse_from_file('README.md')
+
 setup(
     name='typesense',
-    version='0.1.0',
+    version='0.2.2',
     packages=['examples', 'typesense'],
     url='https://typesense.org',
     license='Apache 2.0',
     author='Typesense',
     author_email='contact+typesense@wreally.com',
-    description='Python client for Typesense, an open source and typo tolerant search engine.'
+    description='Python client for Typesense, an open source and typo tolerant search engine.',
+    long_description=long_description,
 )
