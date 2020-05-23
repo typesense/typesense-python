b115406b4b7345f01d0a622f6f47a6c60bae3dea
kishorenc <kishorenc@gmail.com>
2020-05-23 21:28:28 +0530

    Bump to v0.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = parse_from_file('README.md')
 
 setup(
     name='typesense',
-    version='0.4.0',
+    version='0.5.0',
     packages=['examples', 'typesense'],
     install_requires=['requests'],
     url='https://typesense.org',
