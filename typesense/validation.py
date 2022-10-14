8d6a01b013934df48738340d985fb24166a44b89
Orion Reed <orionreedt@gmail.com>
2022-10-14 13:43:23 +0000

    convert bools and ints to strings pre validation

--- typesense/validation.py
+++ typesense/validation.py
@@ -3,5 +3,5 @@ from typesense.exceptions import InvalidParameter
 
 def validate_search(params):
     for key in params:
-        if type(params[key]) is not str:
+        if not isinstance(params[key], str):
             raise InvalidParameter(f"'{key}' field expected a string but was given {type(params[key]).__name__}")
