2996a759cf6adc933350f786844a01a03a1201d4
Kishore Nallan <kishorenc@gmail.com>
2023-08-11 12:37:00 +0530

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = parse_from_file('README.md')
 setup(
     name='typesense',
     python_requires='>=3',
-    version='0.15.1',
+    version='0.16.0',
     packages=['examples', 'typesense'],
     install_requires=['requests'],
     url='https://typesense.org',
