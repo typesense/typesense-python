92a084aa95aef4582b57ea6c766c4f74fe522e06
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-07-26 17:09:07 +0300

    style: add return type for node tests

--- tests/node_test.py
+++ tests/node_test.py
@@ -4,7 +4,7 @@ from src.typesense.configuration import Node
 from src.typesense.exceptions import ConfigError
 
 
-def test_node_initialization():
+def test_node_initialization() -> None:
     node = Node(host="localhost", port=8108, path="/path", protocol="http")
     assert node.host == "localhost"
     assert node.port == 8108
@@ -13,7 +13,7 @@ def test_node_initialization():
     assert node.healthy is True
 
 
-def test_node_from_url():
+def test_node_from_url() -> None:
     node = Node.from_url("http://localhost:8108/path")
     assert node.host == "localhost"
     assert node.port == 8108
@@ -21,21 +21,21 @@ def test_node_from_url():
     assert node.protocol == "http"
 
 
-def test_node_from_url_missing_hostname():
+def test_node_from_url_missing_hostname() -> None:
     with pytest.raises(ConfigError, match="Node URL does not contain the host name."):
         Node.from_url("http://:8108/path")
 
 
-def test_node_from_url_missing_port():
+def test_node_from_url_missing_port() -> None:
     with pytest.raises(ConfigError, match="Node URL does not contain the port."):
         Node.from_url("http://localhost:/path")
 
 
-def test_node_from_url_missing_scheme():
+def test_node_from_url_missing_scheme() -> None:
     with pytest.raises(ConfigError, match="Node URL does not contain the protocol."):
         Node.from_url("//localhost:8108/path")
 
 
-def test_node_url():
+def test_node_url() -> None:
     node = Node(host="localhost", port=8108, path="/path", protocol="http")
     assert node.url() == "http://localhost:8108/path"
