3caf4966bb0b47422008b7e724de909350650e35
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-05 12:04:48 +0300

    feat(synonyms): add type hints for synonyms class
    - Annotating types for methods and attributes.
    - Introducing `SynonymCreateSchema`, `SynonymSchema`, and
      `SynonymsRetrieveSchema` for better schema management.
    - Updating method signatures for better clarity and type enforcement.
    - Ensuring consistent use of schema types in API call responses.

--- src/typesense/synonyms.py
+++ src/typesense/synonyms.py
@@ -1,28 +1,49 @@
+from typesense.api_call import ApiCall
+from typesense.types.synonym import (
+    SynonymCreateSchema,
+    SynonymSchema,
+    SynonymsRetrieveSchema,
+)
+
 from .synonym import Synonym
 
 
 class Synonyms(object):
     RESOURCE_PATH = 'synonyms'
 
-    def __init__(self, api_call, collection_name):
+    def __init__(self, api_call: ApiCall, collection_name: str):
         self.api_call = api_call
         self.collection_name = collection_name
-        self.synonyms = {}
+        self.synonyms: dict[str, Synonym] = {}
 
-    def __getitem__(self, synonym_id):
-        if synonym_id not in self.synonyms:
-            self.synonyms[synonym_id] = Synonym(self.api_call, self.collection_name, synonym_id)
+    def __getitem__(self, synonym_id: str) -> Synonym:
+        if not self.synonyms.get(synonym_id):
+            self.synonyms[synonym_id] = Synonym(
+                self.api_call, self.collection_name, synonym_id
+            )
 
         return self.synonyms[synonym_id]
 
-    def _endpoint_path(self, synonym_id=None):
-        from .collections import Collections
-        synonym_id = synonym_id or ''
-        return u"{0}/{1}/{2}/{3}".format(Collections.RESOURCE_PATH, self.collection_name,
-                                         Synonyms.RESOURCE_PATH, synonym_id)
+    def _endpoint_path(self, synonym_id: str | None = None) -> str:
+        from typesense.collections import Collections
 
-    def upsert(self, id, schema):
-        return self.api_call.put(self._endpoint_path(id), schema)
+        synonym_id = synonym_id or ""
+        return "{0}/{1}/{2}/{3}".format(
+            Collections.RESOURCE_PATH,
+            self.collection_name,
+            Synonyms.RESOURCE_PATH,
+            synonym_id,
+        )
 
-    def retrieve(self):
-        return self.api_call.get(self._endpoint_path())
+    def upsert(self, id: str, schema: SynonymCreateSchema) -> SynonymSchema:
+        response = self.api_call.put(
+            self._endpoint_path(id), body=schema, entity_type=SynonymSchema
+        )
+
+        return response
+
+    def retrieve(self) -> SynonymsRetrieveSchema:
+        response = self.api_call.get(
+            self._endpoint_path(), entity_type=SynonymsRetrieveSchema
+        )
+        return response
