f916eda10a47d0b95b9ffd45c9ca5ccf4f0195a8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-05 11:01:42 +0300

    feat(collections): add type hints for collections class
    - Update Collections class methods with proper type annotations and
    return types.
    - Improve __getitem__ implementation.

--- src/typesense/collections.py
+++ src/typesense/collections.py
@@ -1,21 +1,47 @@
+from __future__ import annotations
+
+import sys
+from email.policy import default
+from typing import TYPE_CHECKING
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
+from typesense.api_call import ApiCall
+from typesense.types.collection import CollectionCreateSchema, CollectionSchema
+
 from .collection import Collection
 
 
 class Collections(object):
     RESOURCE_PATH = '/collections'
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall):
         self.api_call = api_call
-        self.collections = {}
+        self.collections: dict[str, Collection] = {}
 
-    def __getitem__(self, collection_name):
-        if collection_name not in self.collections:
-            self.collections[collection_name] = Collection(self.api_call, collection_name)
+    def __getitem__(self, collection_name: str) -> Collection:
+        if not self.collections.get(collection_name):
+            self.collections[collection_name] = Collection(
+                self.api_call, collection_name
+            )
+        return self.collections[collection_name]
 
-        return self.collections.get(collection_name)
+    def create(self, schema: CollectionCreateSchema) -> CollectionSchema:
+        call: CollectionSchema = self.api_call.post(
+            endpoint=Collections.RESOURCE_PATH,
+            entity_type=CollectionSchema,
+            as_json=True,
+            body=schema,
+        )
+        return call
 
-    def create(self, schema):
-        return self.api_call.post(Collections.RESOURCE_PATH, schema)
-
-    def retrieve(self):
-        return self.api_call.get('{0}'.format(Collections.RESOURCE_PATH))
+    def retrieve(self) -> list[CollectionSchema]:
+        call: list[CollectionSchema] = self.api_call.get(
+            endpoint=Collections.RESOURCE_PATH,
+            as_json=True,
+            entity_type=list[CollectionSchema],
+        )
+        return call
