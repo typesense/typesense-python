bae14dbc306e1c4be92d04593928902c248e62c0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-06 11:07:32 +0300

    feat(alias): add type hints to alias class
    Enhance `Alias` class with type hints and improve method signatures:
    - Add type annotations to methods and attributes
    - Convert _endpoint_path to a property
    - Update retrieve and delete methods to use `AliasSchema`

--- src/typesense/alias.py
+++ src/typesense/alias.py
@@ -1,14 +1,25 @@
+from typesense.api_call import ApiCall
+from typesense.types.alias import AliasSchema
+
+
 class Alias(object):
-    def __init__(self, api_call, name):
+    def __init__(self, api_call: ApiCall, name: str):
         self.api_call = api_call
         self.name = name
 
-    def _endpoint_path(self):
+    @property
+    def _endpoint_path(self) -> str:
         from .aliases import Aliases
-        return u"{0}/{1}".format(Aliases.RESOURCE_PATH, self.name)
 
-    def retrieve(self):
-        return self.api_call.get(self._endpoint_path())
+        return "{0}/{1}".format(Aliases.RESOURCE_PATH, self.name)
 
-    def delete(self):
-        return self.api_call.delete(self._endpoint_path())
+    def retrieve(self) -> AliasSchema:
+        response: AliasSchema = self.api_call.get(
+            self._endpoint_path, entity_type=AliasSchema, as_json=True
+        )
+        return response
+
+    def delete(self) -> AliasSchema:
+        response = self.api_call.delete(self._endpoint_path, entity_type=AliasSchema)
+
+        return response
