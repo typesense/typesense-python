27698cfc6e51d7799d012b5d3dd615558fcea5c7
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-06 11:05:18 +0300

    feat(aliases): add type hints to aliases class
    Enhance `Aliases` class with type hints and use new `AliasSchema` types:
    - Add type annotations to methods and attributes
    - Update upsert and retrieve methods to use new schema types

--- src/typesense/aliases.py
+++ src/typesense/aliases.py
@@ -1,24 +1,37 @@
 from typesense.alias import Alias
+from typesense.api_call import ApiCall
+from typesense.types.alias import AliasCreateSchema, AliasesResponseSchema, AliasSchema
 
 
-class Aliases(object):
-    RESOURCE_PATH = '/aliases'
+class Aliases:
+    RESOURCE_PATH = "/aliases"
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall):
         self.api_call = api_call
-        self.aliases = {}
+        self.aliases: dict[str, Alias] = {}
 
-    def __getitem__(self, name):
-        if name not in self.aliases:
+    def __getitem__(self, name: str) -> Alias:
+        if not self.aliases.get(name):
             self.aliases[name] = Alias(self.api_call, name)
 
         return self.aliases.get(name)
 
-    def _endpoint_path(self, alias_name):
-        return u"{0}/{1}".format(Aliases.RESOURCE_PATH, alias_name)
+    def _endpoint_path(self, alias_name: str) -> str:
+        return "{0}/{1}".format(Aliases.RESOURCE_PATH, alias_name)
 
-    def upsert(self, name, mapping):
-        return self.api_call.put(self._endpoint_path(name), mapping)
+    def upsert(self, name: str, mapping: AliasCreateSchema) -> AliasSchema:
+        response: AliasSchema = self.api_call.put(
+            self._endpoint_path(name),
+            body=mapping,
+            entity_type=AliasSchema,
+        )
 
-    def retrieve(self):
-        return self.api_call.get(Aliases.RESOURCE_PATH)
+        return response
+
+    def retrieve(self) -> AliasesResponseSchema:
+        response: AliasesResponseSchema = self.api_call.get(
+            Aliases.RESOURCE_PATH,
+            as_json=True,
+            entity_type=AliasesResponseSchema,
+        )
+        return response
