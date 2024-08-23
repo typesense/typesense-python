8f82556d21809429e50515ca43bf2a27bf0fe3a2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 15:11:50 +0300

    feat(stopwords_set): add type hints to stopwords set class
    Improve the `StopwordsSet` class with proper type hints:
    - Refactor `StopwordsSet` class to add type annotations for method
    return values.
    - Introduce `StopwordSchema` and `StopwordDeleteSchema` types for
    responses in `retrieve` and `delete` methods, ensuring type safety.
    - Update method signatures to include the `as_json` and `entity_type`
    parameters for better response handling.
    - Improve internal structure by converting `_endpoint_path` to a
    property for cleaner access.

--- src/typesense/stopwords_set.py
+++ src/typesense/stopwords_set.py
@@ -1,14 +1,26 @@
-class StopwordsSet(object):
-    def __init__(self, api_call, stopwords_set_id):
+from typesense.api_call import ApiCall
+from typesense.types.stopword import StopwordDeleteSchema, StopwordsSingleRetrieveSchema
+
+
+class StopwordsSet:
+    def __init__(self, api_call: ApiCall, stopwords_set_id: str) -> None:
         self.stopwords_set_id = stopwords_set_id
         self.api_call = api_call
 
-    def _endpoint_path(self):
+    @property
+    def _endpoint_path(self) -> str:
         from .stopwords import Stopwords
-        return u"{0}/{1}".format(Stopwords.RESOURCE_PATH, self.stopwords_set_id)
 
-    def retrieve(self):
-        return self.api_call.get(self._endpoint_path())
+        return "{0}/{1}".format(Stopwords.RESOURCE_PATH, self.stopwords_set_id)
 
-    def delete(self):
-        return self.api_call.delete(self._endpoint_path())
+    def retrieve(self) -> StopwordsSingleRetrieveSchema:
+        response: StopwordsSingleRetrieveSchema = self.api_call.get(
+            self._endpoint_path, as_json=True, entity_type=StopwordsSingleRetrieveSchema
+        )
+        return response
+
+    def delete(self) -> StopwordDeleteSchema:
+        response: StopwordDeleteSchema = self.api_call.delete(
+            self._endpoint_path, entity_type=StopwordDeleteSchema
+        )
+        return response
