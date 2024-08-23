90e0912282b30fad73b548b0a2bf1d38480aa03b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 15:01:06 +0300

    feat(analytics): add type hints to analytics class
    Improve the `Analytics` class with proper type hints:
    - Add type hint to the `api_call` parameter in the `Analytics` class
    constructor.

--- src/typesense/analytics.py
+++ src/typesense/analytics.py
@@ -1,6 +1,8 @@
+from typesense.api_call import ApiCall
+
 from .analytics_rules import AnalyticsRules
 
 
 class Analytics(object):
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall) -> None:
         self.rules = AnalyticsRules(api_call)
