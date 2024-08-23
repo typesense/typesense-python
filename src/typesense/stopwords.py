c4ecb152778ff4228680b122dc89087dfe0f4f85
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 15:14:00 +0300

    feat(stopwords): add type hints to stopwords class
    - Add type annotations to the `Stopwords` class methods to enhance code
    clarity and type safety.
    - Update the `__getitem__` method to ensure proper initialization and
    retrieval of `StopwordsSet` instances, with type safety and improved
    dictionary handling.
    - Refactor the `upsert` and `retrieve` methods to utilize explicit
    response types, ensuring the API calls return correctly typed data.
    - Improve the overall structure of API calls by including entity type
    specification and JSON parsing in the methods.

--- src/typesense/stopwords.py
+++ src/typesense/stopwords.py
@@ -1,21 +1,48 @@
+import sys
+
+from typesense.api_call import ApiCall
+from typesense.types.stopword import (
+    StopwordCreateSchema,
+    StopwordSchema,
+    StopwordsRetrieveSchema,
+)
+
 from .stopwords_set import StopwordsSet
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
 
 
 class Stopwords(object):
-    RESOURCE_PATH = '/stopwords'
+    RESOURCE_PATH = "/stopwords"
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall):
         self.api_call = api_call
-        self.stopwords_sets = {}
+        self.stopwords_sets: typing.Dict[str, StopwordsSet] = {}
 
-    def __getitem__(self, stopwords_set_id):
-        if stopwords_set_id not in self.stopwords_sets:
-            self.stopwords_sets[stopwords_set_id] = StopwordsSet(self.api_call, stopwords_set_id)
+    def __getitem__(self, stopwords_set_id: str) -> StopwordsSet:
+        if not self.stopwords_sets.get(stopwords_set_id):
+            self.stopwords_sets[stopwords_set_id] = StopwordsSet(
+                self.api_call, stopwords_set_id
+            )
+        return self.stopwords_sets[stopwords_set_id]
 
-        return self.stopwords_sets.get(stopwords_set_id)
+    def upsert(
+        self, stopwords_set_id: str, stopwords_set: StopwordCreateSchema
+    ) -> StopwordSchema:
+        response: StopwordSchema = self.api_call.put(
+            "{}/{}".format(Stopwords.RESOURCE_PATH, stopwords_set_id),
+            body=stopwords_set,
+            entity_type=StopwordSchema,
+        )
+        return response
 
-    def upsert(self, stopwords_set_id, stopwords_set):
-        return self.api_call.put('{}/{}'.format(Stopwords.RESOURCE_PATH, stopwords_set_id), stopwords_set)
-
-    def retrieve(self):
-        return self.api_call.get('{0}'.format(Stopwords.RESOURCE_PATH))
+    def retrieve(self) -> StopwordsRetrieveSchema:
+        response: StopwordsRetrieveSchema = self.api_call.get(
+            "{0}".format(Stopwords.RESOURCE_PATH),
+            as_json=True,
+            entity_type=StopwordsRetrieveSchema,
+        )
+        return response
