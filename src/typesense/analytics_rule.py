01325d98361e6df7f5e80dd7b7fcda055bcf2d38
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 14:44:52 +0300

    feat(analytics_rule): add type hints to analytics rule class
    Improve the `AnalyticsRule` class with proper type hints:
    - Add type annotations to methods and parameters
    - Update delete and retrieve methods with specific return types

--- src/typesense/analytics_rule.py
+++ src/typesense/analytics_rule.py
@@ -1,14 +1,39 @@
+import typing
+from urllib import response
+
+from typesense.api_call import ApiCall
+from typesense.types.analytics_rule import (
+    RuleDeleteSchema,
+    RuleSchemaForCounters,
+    RuleSchemaForQueries,
+)
+
+
 class AnalyticsRule(object):
-    def __init__(self, api_call, rule_id):
+    def __init__(self, api_call: ApiCall, rule_id: str):
         self.api_call = api_call
         self.rule_id = rule_id
 
-    def _endpoint_path(self):
+    @property
+    def _endpoint_path(self) -> str:
         from .analytics_rules import AnalyticsRules
-        return u"{0}/{1}".format(AnalyticsRules.RESOURCE_PATH, self.rule_id)
 
-    def retrieve(self):
-        return self.api_call.get(self._endpoint_path())
+        return "{0}/{1}".format(AnalyticsRules.RESOURCE_PATH, self.rule_id)
 
-    def delete(self):
-        return self.api_call.delete(self._endpoint_path())
+    def retrieve(
+        self,
+    ) -> typing.Union[RuleSchemaForQueries, RuleSchemaForCounters]:
+        response: typing.Union[RuleSchemaForQueries, RuleSchemaForCounters] = (
+            self.api_call.get(
+                self._endpoint_path,
+                entity_type=typing.Union[RuleSchemaForQueries, RuleSchemaForCounters],
+                as_json=True,
+            )
+        )
+        return response
+
+    def delete(self) -> RuleDeleteSchema:
+        response: RuleDeleteSchema = self.api_call.delete(
+            self._endpoint_path, entity_type=RuleDeleteSchema
+        )
+        return response
