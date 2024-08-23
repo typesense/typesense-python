df1de4061a4e1aa4d759ec7bab361a3fa336b002
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 14:41:35 +0300

    feat(analytics_rules): add type hints to analytics rules class
    Improve the `AnalyticsRules` class with proper type hints:
    - Add type annotations to methods and parameters
    - Update create, upsert, and retrieve methods with specific return types
    - Refactor __getitem__ for better type safety

--- src/typesense/analytics_rules.py
+++ src/typesense/analytics_rules.py
@@ -1,26 +1,72 @@
+import sys
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
+from typesense.api_call import ApiCall
+from typesense.types.analytics_rule import (
+    RuleCreateSchemaForQueries,
+    RuleSchemaForCounters,
+    RuleSchemaForQueries,
+    RulesRetrieveSchema,
+)
+
 from .analytics_rule import AnalyticsRule
 
 
 class AnalyticsRules(object):
-    RESOURCE_PATH = '/analytics/rules'
+    RESOURCE_PATH = "/analytics/rules"
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall):
         self.api_call = api_call
-        self.rules = {}
+        self.rules: typing.Dict[str, AnalyticsRule] = {}
 
-    def __getitem__(self, rule_id):
-        if rule_id not in self.rules:
+    def __getitem__(self, rule_id: str) -> AnalyticsRule:
+        if not self.rules.get(rule_id):
             self.rules[rule_id] = AnalyticsRule(self.api_call, rule_id)
 
         return self.rules[rule_id]
 
-    def create(self, rule, params=None):
-        params = params or {}
-        return self.api_call.post(AnalyticsRules.RESOURCE_PATH, rule, params)
+    def create(
+        self,
+        rule: typing.Union[RuleSchemaForCounters, RuleCreateSchemaForQueries],
+        params: typing.Union[
+            typing.Dict[str, typing.Union[str, int, bool]], None
+        ] = None,
+    ) -> typing.Union[RuleSchemaForCounters, RuleCreateSchemaForQueries]:
+        response: typing.Union[RuleSchemaForCounters, RuleCreateSchemaForQueries] = (
+            self.api_call.post(
+                AnalyticsRules.RESOURCE_PATH,
+                body=rule,
+                params=params,
+                as_json=True,
+                entity_type=typing.Union[
+                    RuleSchemaForCounters, RuleCreateSchemaForQueries
+                ],
+            )
+        )
+        return response
 
-    def upsert(self, id, rule):
-        return self.api_call.put(u"{0}/{1}".format(AnalyticsRules.RESOURCE_PATH, id), rule)
+    def upsert(
+        self,
+        id: str,
+        rule: typing.Union[RuleCreateSchemaForQueries, RuleSchemaForCounters],
+    ) -> typing.Union[RuleSchemaForCounters, RuleCreateSchemaForQueries]:
+        response = self.api_call.put(
+            "{0}/{1}".format(AnalyticsRules.RESOURCE_PATH, id),
+            body=rule,
+            entity_type=typing.Union[RuleSchemaForQueries, RuleSchemaForCounters],
+        )
+        return typing.cast(
+            typing.Union[RuleSchemaForCounters, RuleCreateSchemaForQueries], response
+        )
 
-    def retrieve(self):
-        return self.api_call.get(AnalyticsRules.RESOURCE_PATH)
-
+    def retrieve(self) -> RulesRetrieveSchema:
+        response: RulesRetrieveSchema = self.api_call.get(
+            AnalyticsRules.RESOURCE_PATH,
+            as_json=True,
+            entity_type=RulesRetrieveSchema,
+        )
+        return response
