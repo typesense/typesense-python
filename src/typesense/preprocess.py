14d5b1869411ee11527969f3b9e90047d4263e7c
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-23 16:47:43 +0300

    feat(preprocess): add types for preprocess operations
    - Update the `stringify` function to improve compatibility   with Python
    versions above 3.11.
    - Introduce type aliases for clearer type definitions of   parameters
    and their string representations.

--- src/typesense/preprocess.py
+++ src/typesense/preprocess.py
@@ -1,7 +1,30 @@
 def stringify_search_params(params):
     return {key:stringify(val) for key, val in params.items()}
+import sys
 
 def stringify(val):
+
+if sys.version_info > (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
+
+_ListTypes = typing.List[typing.Union[str, int, bool]]
+
+_Types = typing.Union[int, str, bool]
+
+ParamSchema: typing.TypeAlias = typing.Dict[
+    str,
+    typing.Union[
+        _Types,
+        _ListTypes,
+    ],
+]
+
+
+StringifiedParamSchema: typing.TypeAlias = typing.Dict[str, str]
+
     if isinstance(val, bool) or isinstance(val, int):
         return str(val).lower()
     else:
