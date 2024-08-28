9d6a25ebb19ae6eb05e4bdee11b29aea325ff4ee
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-28 15:10:14 +0300

    feat(multi-search): add type hints to multi search class
    - Add type annotations to the `MultiSearch` class, improving code clarity.
    - Introduce `MultiSearchRequestSchema` and `MultiSearchResponse` types for
    better structure.
    - Refactor `perform` method to handle request and response more robustly.
    - Enhance the `perform` method to stringify search parameters before sending
    the request.
    - Ensure compatibility with Python versions prior to 3.11 using conditional
    imports.
    
    This refactor improves the robustness and readability of the `MultiSearch`
    class, ensuring better type safety and request handling.

--- src/typesense/multi_search.py
+++ src/typesense/multi_search.py
@@ -1,9 +1,37 @@
+import sys
+
+from typesense.api_call import ApiCall
+from typesense.preprocess import stringify_search_params
+from typesense.types.document import MultiSearchCommonParameters
+from typesense.types.multi_search import MultiSearchRequestSchema, MultiSearchResponse
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
 
 class MultiSearch(object):
-    RESOURCE_PATH = '/multi_search'
+    RESOURCE_PATH = "/multi_search"
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall) -> None:
         self.api_call = api_call
 
-    def perform(self, search_queries, common_params):
-        return self.api_call.post(MultiSearch.RESOURCE_PATH, search_queries, common_params)
+    def perform(
+        self,
+        search_queries: MultiSearchRequestSchema,
+        common_params: typing.Union[MultiSearchCommonParameters, None] = None,
+    ) -> MultiSearchResponse:
+        stringified_search_params = [
+            stringify_search_params(search_params)
+            for search_params in search_queries.get("searches")
+        ]
+        search_body = {"searches": stringified_search_params}
+        response: MultiSearchResponse = self.api_call.post(
+            MultiSearch.RESOURCE_PATH,
+            body=search_body,
+            params=common_params,
+            as_json=True,
+            entity_type=MultiSearchResponse,
+        )
+        return response
