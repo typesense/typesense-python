f6ac2244f1cfc193ebd09bd73ce6579b0011dc7c
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-28 15:08:16 +0300

    feat(multi-search): add types for multi search
    - Introduce `MultiSearchRequestSchema` to define the schema for
    multi-search requests.
    - Add `MultiSearchResponse` to represent the structure of
    multi-search responses.
    - Ensure compatibility with Python 3.11 by conditionally importing
    `typing` or `typing_extensions`.
    - Enhance type safety and clarity in handling multi-search operations.
    
    This update provides clear and structured types for multi-search functionality,
    improving code readability and reducing potential errors in handling
    multi-search requests and responses.

---
+++ src/typesense/types/multi_search.py
@@ -0,0 +1,32 @@
+"""Types for multi-search."""
+
+import sys
+
+from typesense.types.document import MultiSearchParameters, SearchResponse
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
+
+class MultiSearchResponse(typing.TypedDict):
+    """
+    Response schema for multi-search.
+
+    Attributes:
+        results (list[SearchResponse]): The search results.
+    """
+
+    results: typing.List[SearchResponse[typing.Any]]  # noqa: WPS110
+
+
+class MultiSearchRequestSchema(typing.TypedDict):
+    """
+    Schema for multi-search request.
+
+    Attributes:
+        searches (list[MultiSearchParameters]): The search parameters.
+    """
+
+    searches: typing.List[MultiSearchParameters]
