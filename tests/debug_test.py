fba56a8c35fcdd89661d949eae60e311e22bd433
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-30 17:45:35 +0300

    test(debug): add tests for debug module
    - Added unit tests to ensure the `Debug` class is initialized correctly.
    - Verified that the `retrieve()` method functions properly, both with mocked responses and actual server data.
    - Used assertions to check consistency in API call configurations and response handling.

---
+++ tests/debug_test.py
@@ -0,0 +1,52 @@
+"""Tests for the Debug class."""
+
+from __future__ import annotations
+
+import requests_mock
+
+from tests.utils.object_assertions import assert_match_object, assert_object_lists_match
+from typesense.api_call import ApiCall
+from typesense.debug import Debug
+from typesense.types.debug import DebugResponseSchema
+
+
+def test_init(fake_api_call: ApiCall) -> None:
+    """Test that the Debug object is initialized correctly."""
+    debug = Debug(
+        fake_api_call,
+    )
+
+    assert_match_object(debug.api_call, fake_api_call)
+    assert_object_lists_match(debug.api_call.nodes, fake_api_call.nodes)
+    assert_match_object(
+        debug.api_call.config.nearest_node,
+        fake_api_call.config.nearest_node,
+    )
+    assert debug.RESOURCE_PATH == "/debug"  # noqa: WPS437
+
+
+def test_retrieve(fake_debug: Debug) -> None:
+    """Test that the Debug object can retrieve a debug."""
+    json_response: DebugResponseSchema = {"state": 1, "version": "27.0"}
+
+    with requests_mock.Mocker() as mock:
+        mock.get(
+            "/debug",
+            json=json_response,
+        )
+
+        response = fake_debug.retrieve()
+
+        assert len(mock.request_history) == 1
+        assert mock.request_history[0].method == "GET"
+        assert mock.request_history[0].url == "http://nearest:8108/debug"
+        assert response == json_response
+
+
+def test_actual_retrieve(actual_debug: Debug) -> None:
+    """Test that the Debug object can retrieve a debug on Typesense server."""
+    json_response: DebugResponseSchema = {"state": 1, "version": "27.0"}
+
+    response = actual_debug.retrieve()
+
+    assert response == json_response
