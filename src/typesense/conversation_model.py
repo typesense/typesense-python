7337c3355b4dd8283880f4ea899ea86fc56e4ed2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-30 13:15:45 +0300

    feat(convo-model): add type hints to convo model class
    Refactor `ConversationModel` methods and add type annotations
    
    - Add type annotations for `__init__`, `_endpoint_path`, `retrieve`,
    `update`, and `delete` methods
    - Convert `_endpoint_path` method to a `@property`
    - Refactor `retrieve`, `update`, and `delete` methods to use typed
    responses
    - Ensure correct request handling by updating API call methods to
    return strongly typed schemas (`ConversationModelSchema`,
    `ConversationModelCreateSchema`, and `ConversationModelDeleteSchema`)

--- src/typesense/conversation_model.py
+++ src/typesense/conversation_model.py
@@ -1,17 +1,41 @@
+from typesense.api_call import ApiCall
+from typesense.types.conversations_model import (
+    ConversationModelCreateSchema,
+    ConversationModelDeleteSchema,
+    ConversationModelSchema,
+)
+
+
 class ConversationModel(object):
-    def __init__(self, api_call, model_id):
+    def __init__(self, api_call: ApiCall, model_id: str) -> None:
         self.model_id = model_id
         self.api_call = api_call
 
-    def _endpoint_path(self):
+    @property
+    def _endpoint_path(self) -> str:
         from .conversations_models import ConversationsModels
-        return u"{0}/{1}".format(ConversationsModels.RESOURCE_PATH, self.model_id)
 
-    def retrieve(self):
-        return self.api_call.get(self._endpoint_path())
+        return "{0}/{1}".format(ConversationsModels.RESOURCE_PATH, self.model_id)
 
-    def update(self, model):
-        return self.api_call.put(self._endpoint_path(), model)
+    def retrieve(self) -> ConversationModelSchema:
+        response = self.api_call.get(
+            self._endpoint_path,
+            as_json=True,
+            entity_type=ConversationModelSchema,
+        )
+        return response
 
-    def delete(self):
-        return self.api_call.delete(self._endpoint_path())
+    def update(self, model: ConversationModelCreateSchema) -> ConversationModelSchema:
+        response: ConversationModelSchema = self.api_call.put(
+            self._endpoint_path,
+            body=model,
+            entity_type=ConversationModelSchema,
+        )
+        return response
+
+    def delete(self) -> ConversationModelDeleteSchema:
+        response: ConversationModelDeleteSchema = self.api_call.delete(
+            self._endpoint_path,
+            entity_type=ConversationModelDeleteSchema,
+        )
+        return response
