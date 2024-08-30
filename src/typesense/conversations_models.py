7a4c4511b9fccc7bd51027a19f5f75c5604a1956
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-30 13:11:35 +0300

    feat(convo-models): add type hints to convo models
    Enhance ConversationsModels with type annotations
    
    - Add type hints to `ConversationsModels` class methods
    - Improve `create()` and `retrieve()` methods with specific types
    - Use `ConversationModelCreateSchema` and `ConversationModelSchema`
    - Implement `typing.Dict` for `conversations_models` attribute
    - Update `__getitem__` method with proper return type
    - Ensure compatibility with Python versions before 3.11

--- src/typesense/conversations_models.py
+++ src/typesense/conversations_models.py
@@ -1,21 +1,48 @@
+import sys
+
+from typesense.api_call import ApiCall
+from typesense.types.conversations_model import (
+    ConversationModelCreateSchema,
+    ConversationModelSchema,
+)
+
+if sys.version_info > (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
 from .conversation_model import ConversationModel
 
 
 class ConversationsModels(object):
-    RESOURCE_PATH = '/conversations/models'
+    RESOURCE_PATH = "/conversations/models"
 
-    def __init__(self, api_call):
+    def __init__(self, api_call: ApiCall) -> None:
         self.api_call = api_call
-        self.conversations_models = {}
+        self.conversations_models: typing.Dict[str, ConversationModel] = {}
 
-    def __getitem__(self, model_id):
+    def __getitem__(self, model_id: str) -> ConversationModel:
         if model_id not in self.conversations_models:
-            self.conversations_models[model_id] = ConversationModel(self.api_call, model_id)
+            self.conversations_models[model_id] = ConversationModel(
+                self.api_call,
+                model_id,
+            )
 
-        return self.conversations_models.get(model_id)
+        return self.conversations_models[model_id]
 
-    def create(self, model):
-        return self.api_call.post(ConversationsModels.RESOURCE_PATH, model)
+    def create(self, model: ConversationModelCreateSchema) -> ConversationModelSchema:
+        response = self.api_call.post(
+            endpoint=ConversationsModels.RESOURCE_PATH,
+            entity_type=ConversationModelSchema,
+            as_json=True,
+            body=model,
+        )
+        return response
 
-    def retrieve(self):
-        return self.api_call.get(ConversationsModels.RESOURCE_PATH)
+    def retrieve(self) -> typing.List[ConversationModelSchema]:
+        response: typing.List[ConversationModelSchema] = self.api_call.get(
+            endpoint=ConversationsModels.RESOURCE_PATH,
+            entity_type=typing.List[ConversationModelSchema],
+            as_json=True,
+        )
+        return response
