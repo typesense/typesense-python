2b5d8c29bc72c50929ad54abd75fd77450ae5c5b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-08-30 19:08:16 +0300

    feat(validation): add type hints to validation functions
    - Update the `validate_search` function to explicitly define its
      parameter type as `typing.Mapping[str, str]`, ensuring better
      type safety and clarity.
    - Improve error handling by refining the `InvalidParameter`
      exception message, making it more informative by indicating
      the expected type and the actual type provided.
    
    These changes increase robustness and readability in parameter
    validation for search operations.

--- src/typesense/validation.py
+++ src/typesense/validation.py
@@ -1,7 +1,18 @@
+from __future__ import annotations
+
+import sys
+
+if sys.version_info > (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
 from typesense.exceptions import InvalidParameter
 
 
-def validate_search(params):
+def validate_search(params: typing.Mapping[str, str]) -> None:
     for key in params:
         if not isinstance(params[key], str):
-            raise InvalidParameter(f"'{key}' field expected a string but was given {type(params[key]).__name__}")
+            raise InvalidParameter(
+                f"'{key}' field expected a string but was given {type(params[key]).__name__}"
+            )
