24dec9924b9b1115d6c65cbac2cf55ce353aeb5f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-09-09 19:59:26 +0300

    refactor(convo-model): format convo model classes based on linting rules

--- src/typesense/conversation_model.py
+++ src/typesense/conversation_model.py
@@ -1,3 +1,27 @@
+"""
+This module provides functionality for managing individual conversation models in Typesense.
+
+Classes:
+    - ConversationModel: Handles operations related to a specific conversation model.
+
+Methods:
+    - __init__: Initializes the ConversationModel object.
+    - _endpoint_path: Constructs the API endpoint path for this specific conversation model.
+    - retrieve: Retrieves the details of this specific conversation model.
+    - update: Updates this specific conversation model.
+    - delete: Deletes this specific conversation model.
+
+The ConversationModel class interacts with the Typesense API to manage operations on a
+specific conversation model. It provides methods to retrieve, update,
+and delete individual models.
+
+For more information on conversation models and RAG, refer to the Conversational Search
+[documentation](https://typesense.org/docs/27.0/api/conversational-search-rag.html)
+
+This module uses type hinting and is compatible with Python 3.11+ as well as earlier
+versions through the use of the typing_extensions library.
+"""
+
 from typesense.api_call import ApiCall
 from typesense.types.conversations_model import (
     ConversationModelCreateSchema,
@@ -6,18 +30,36 @@ from typesense.types.conversations_model import (
 )
 
 
-class ConversationModel(object):
+class ConversationModel:
+    """
+    Class for managing individual conversation models in Typesense.
+
+    This class provides methods to interact with a specific conversation model,
+    including retrieving, updating, and deleting it.
+
+    Attributes:
+        model_id (str): The ID of the conversation model.
+        api_call (ApiCall): The API call object for making requests.
+    """
+
     def __init__(self, api_call: ApiCall, model_id: str) -> None:
+        """
+        Initialize the ConversationModel object.
+
+        Args:
+            api_call (ApiCall): The API call object for making requests.
+            model_id (str): The ID of the conversation model.
+        """
         self.model_id = model_id
         self.api_call = api_call
 
-    @property
-    def _endpoint_path(self) -> str:
-        from .conversations_models import ConversationsModels
-
-        return "{0}/{1}".format(ConversationsModels.RESOURCE_PATH, self.model_id)
-
     def retrieve(self) -> ConversationModelSchema:
+        """
+        Retrieve this specific conversation model.
+
+        Returns:
+            ConversationModelSchema: The schema containing the conversation model details.
+        """
         response = self.api_call.get(
             self._endpoint_path,
             as_json=True,
@@ -26,6 +68,16 @@ class ConversationModel(object):
         return response
 
     def update(self, model: ConversationModelCreateSchema) -> ConversationModelSchema:
+        """
+        Update this specific conversation model.
+
+        Args:
+            model (ConversationModelCreateSchema):
+              The schema containing the updated model details.
+
+        Returns:
+            ConversationModelSchema: The schema containing the updated conversation model.
+        """
         response: ConversationModelSchema = self.api_call.put(
             self._endpoint_path,
             body=model,
@@ -34,8 +86,26 @@ class ConversationModel(object):
         return response
 
     def delete(self) -> ConversationModelDeleteSchema:
+        """
+        Delete this specific conversation model.
+
+        Returns:
+            ConversationModelDeleteSchema: The schema containing the deletion response.
+        """
         response: ConversationModelDeleteSchema = self.api_call.delete(
             self._endpoint_path,
             entity_type=ConversationModelDeleteSchema,
         )
         return response
+
+    @property
+    def _endpoint_path(self) -> str:
+        """
+        Construct the API endpoint path for this specific conversation model.
+
+        Returns:
+            str: The constructed endpoint path.
+        """
+        from typesense.conversations_models import ConversationsModels
+
+        return "/".join([ConversationsModels.resource_path, self.model_id])
--- src/typesense/conversations_models.py
+++ src/typesense/conversations_models.py
@@ -1,3 +1,32 @@
+"""
+This module provides functionality for managing conversation models in Typesense.
+
+Classes:
+    - ConversationsModels: Handles operations related to conversation models.
+
+Methods:
+    - __init__: Initializes the ConversationsModels object.
+    - __getitem__: Retrieves or creates a ConversationModel object for a given model_id.
+    - create: Creates a new conversation model.
+    - retrieve: Retrieves all conversation models.
+
+Attributes:
+    - resource_path: The API resource path for conversation models operations.
+
+The ConversationsModels class interacts with the Typesense API to manage
+conversation model operations.
+
+It provides methods to create and retrieve conversation models, as well as access
+individual ConversationModel objects.
+
+
+For more information on conversation models and RAG, refer to the Conversational Search
+[documentation](https://typesense.org/docs/27.0/api/conversational-search-rag.html)
+
+This module uses type hinting and is compatible with Python 3.11+ as well as earlier
+versions through the use of the typing_extensions library.
+"""
+
 import sys
 
 from typesense.api_call import ApiCall
@@ -11,28 +40,65 @@ if sys.version_info > (3, 11):
 else:
     import typing_extensions as typing
 
-from .conversation_model import ConversationModel
+from typesense.conversation_model import ConversationModel
 
 
 class ConversationsModels(object):
-    RESOURCE_PATH = "/conversations/models"
+    """
+    Class for managing conversation models in Typesense.
+
+    This class provides methods to interact with conversation models, including
+    creating, retrieving, and accessing individual models.
+
+    Attributes:
+        resource_path (str): The API resource path for conversation models operations.
+        api_call (ApiCall): The API call object for making requests.
+        conversations_models (Dict[str, ConversationModel]):
+            A dictionary of ConversationModel objects.
+    """
+
+    resource_path: typing.Final[str] = "/conversations/models"
 
     def __init__(self, api_call: ApiCall) -> None:
+        """
+        Initialize the ConversationsModels object.
+
+        Args:
+            api_call (ApiCall): The API call object for making requests.
+        """
         self.api_call = api_call
         self.conversations_models: typing.Dict[str, ConversationModel] = {}
 
     def __getitem__(self, model_id: str) -> ConversationModel:
+        """
+        Get or create a ConversationModel object for a given model_id.
+
+        Args:
+            model_id (str): The ID of the conversation model.
+
+        Returns:
+            ConversationModel: The ConversationModel object for the given ID.
+        """
         if model_id not in self.conversations_models:
             self.conversations_models[model_id] = ConversationModel(
                 self.api_call,
                 model_id,
             )
-
         return self.conversations_models[model_id]
 
     def create(self, model: ConversationModelCreateSchema) -> ConversationModelSchema:
+        """
+        Create a new conversation model.
+
+        Args:
+            model (ConversationModelCreateSchema):
+                The schema for creating the conversation model.
+
+        Returns:
+            ConversationModelSchema: The created conversation model.
+        """
         response = self.api_call.post(
-            endpoint=ConversationsModels.RESOURCE_PATH,
+            endpoint=ConversationsModels.resource_path,
             entity_type=ConversationModelSchema,
             as_json=True,
             body=model,
@@ -40,8 +106,14 @@ class ConversationsModels(object):
         return response
 
     def retrieve(self) -> typing.List[ConversationModelSchema]:
+        """
+        Retrieve all conversation models.
+
+        Returns:
+            List[ConversationModelSchema]: A list of all conversation models.
+        """
         response: typing.List[ConversationModelSchema] = self.api_call.get(
-            endpoint=ConversationsModels.RESOURCE_PATH,
+            endpoint=ConversationsModels.resource_path,
             entity_type=typing.List[ConversationModelSchema],
             as_json=True,
         )
