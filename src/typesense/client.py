af2dec92882d8ce2e11286d8d821f6e63cb1bf3f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-09-09 15:29:56 +0300

    feat(client): add type hints to client class
    - Add type annotations to Client class and its methods
    - Introduce new get_collection method for typed Collection retrieval
    - Refactor and organize imports
    - Remove duplicate imports
    - Improve type safety with conditional typing imports for Python 3.11+

--- src/typesense/client.py
+++ src/typesense/client.py
@@ -1,20 +1,34 @@
+import sys
+
+from typesense.types.document import DocumentSchema
+
+if sys.version_info >= (3, 11):
+    import typing
+else:
+    import typing_extensions as typing
+
+from typesense.collection import Collection
+
 from .aliases import Aliases
+from .analytics import Analytics
+from .api_call import ApiCall
+from .collections import Collections
+from .configuration import ConfigDict, Configuration
 from .conversations_models import ConversationsModels
 from .debug import Debug
-from .collections import Collections
-from .multi_search import MultiSearch
 from .keys import Keys
+from .multi_search import MultiSearch
 from .operations import Operations
-from .configuration import Configuration
-from .api_call import ApiCall
-from .analytics import Analytics
 from .stopwords import Stopwords
 
+TDoc = typing.TypeVar("TDoc", bound=DocumentSchema)
+
+
 class Client(object):
-    def __init__(self, config_dict):
+    def __init__(self, config_dict: ConfigDict) -> None:
         self.config = Configuration(config_dict)
         self.api_call = ApiCall(self.config)
-        self.collections = Collections(self.api_call)
+        self.collections: Collections[DocumentSchema] = Collections(self.api_call)
         self.multi_search = MultiSearch(self.api_call)
         self.keys = Keys(self.api_call)
         self.aliases = Aliases(self.api_call)
@@ -23,3 +37,14 @@ class Client(object):
         self.debug = Debug(self.api_call)
         self.stopwords = Stopwords(self.api_call)
         self.conversations_models = ConversationsModels(self.api_call)
+
+    def typed_collection(
+        self,
+        *,
+        model: typing.Type[TDoc],
+        name: typing.Union[str, None] = None,
+    ) -> Collection[TDoc]:
+        if name is None:
+            name = model.__name__.lower()
+        collection: Collection[TDoc] = self.collections[name]
+        return collection
