9b8ca5535a8730fba264fb97af99c7be2734e9f3
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-09-09 20:20:36 +0300

    refactor(stopwords): format stopword classes based on linting rules

--- src/typesense/stopwords.py
+++ src/typesense/stopwords.py
@@ -1,13 +1,35 @@
+"""
+This module provides functionality for managing stopwords in Typesense.
+
+Classes:
+    - Stopwords: Handles operations related to stopwords and stopword sets.
+
+Methods:
+    - __init__: Initializes the Stopwords object.
+    - __getitem__: Retrieves or creates a StopwordsSet object for a given stopwords_set_id.
+    - upsert: Creates or updates a stopwords set.
+    - retrieve: Retrieves all stopwords sets.
+
+Attributes:
+    - RESOURCE_PATH: The API resource path for stopwords operations.
+
+The Stopwords class interacts with the Typesense API to manage stopwords operations.
+It provides methods to create, update, and retrieve stopwords sets, as well as access
+individual StopwordsSet objects.
+
+This module uses type hinting and is compatible with Python 3.11+ as well as earlier
+versions through the use of the typing_extensions library.
+"""
+
 import sys
 
 from typesense.api_call import ApiCall
+from typesense.stopwords_set import StopwordsSet
 from typesense.types.stopword import (
     StopwordCreateSchema,
     StopwordSchema,
     StopwordsRetrieveSchema,
 )
-
-from .stopwords_set import StopwordsSet
 
 if sys.version_info >= (3, 11):
     import typing
@@ -15,33 +37,80 @@ else:
     import typing_extensions as typing
 
 
-class Stopwords(object):
-    RESOURCE_PATH = "/stopwords"
+class Stopwords:
+    """
+    Class for managing stopwords in Typesense.
+
+    This class provides methods to interact with stopwords and stopwords sets, including
+    creating, updating, retrieving, and accessing individual stopwords sets.
+
+    Attributes:
+        RESOURCE_PATH (str): The API resource path for stopwords operations.
+        api_call (ApiCall): The API call object for making requests.
+        stopwords_sets (Dict[str, StopwordsSet]): A dictionary of StopwordsSet objects.
+    """
+
+    resource_path: typing.Final[str] = "/stopwords"
 
     def __init__(self, api_call: ApiCall):
+        """
+        Initialize the Stopwords object.
+
+        Args:
+            api_call (ApiCall): The API call object for making requests.
+        """
         self.api_call = api_call
         self.stopwords_sets: typing.Dict[str, StopwordsSet] = {}
 
     def __getitem__(self, stopwords_set_id: str) -> StopwordsSet:
+        """
+        Get or create a StopwordsSet object for a given stopwords_set_id.
+
+        Args:
+            stopwords_set_id (str): The ID of the stopwords set.
+
+        Returns:
+            StopwordsSet: The StopwordsSet object for the given ID.
+        """
         if not self.stopwords_sets.get(stopwords_set_id):
             self.stopwords_sets[stopwords_set_id] = StopwordsSet(
-                self.api_call, stopwords_set_id
+                self.api_call,
+                stopwords_set_id,
             )
         return self.stopwords_sets[stopwords_set_id]
 
     def upsert(
-        self, stopwords_set_id: str, stopwords_set: StopwordCreateSchema
+        self,
+        stopwords_set_id: str,
+        stopwords_set: StopwordCreateSchema,
     ) -> StopwordSchema:
+        """
+        Create or update a stopwords set.
+
+        Args:
+            stopwords_set_id (str): The ID of the stopwords set to upsert.
+            stopwords_set (StopwordCreateSchema):
+                The schema for creating or updating the stopwords set.
+
+        Returns:
+            StopwordSchema: The created or updated stopwords set.
+        """
         response: StopwordSchema = self.api_call.put(
-            "{}/{}".format(Stopwords.RESOURCE_PATH, stopwords_set_id),
+            "/".join([Stopwords.resource_path, stopwords_set_id]),
             body=stopwords_set,
             entity_type=StopwordSchema,
         )
         return response
 
     def retrieve(self) -> StopwordsRetrieveSchema:
+        """
+        Retrieve all stopwords sets.
+
+        Returns:
+            StopwordsRetrieveSchema: The schema containing all stopwords sets.
+        """
         response: StopwordsRetrieveSchema = self.api_call.get(
-            "{0}".format(Stopwords.RESOURCE_PATH),
+            Stopwords.resource_path,
             as_json=True,
             entity_type=StopwordsRetrieveSchema,
         )
--- src/typesense/stopwords_set.py
+++ src/typesense/stopwords_set.py
@@ -1,26 +1,87 @@
+"""
+This module provides functionality for managing individual stopwords sets in Typesense.
+
+Classes:
+    - StopwordsSet: Handles operations related to a specific stopwords set.
+
+Methods:
+    - __init__: Initializes the StopwordsSet object.
+    - retrieve: Retrieves the details of this specific stopwords set.
+    - delete: Deletes this specific stopwords set.
+    - _endpoint_path: Constructs the API endpoint path for this specific stopwords set.
+
+The StopwordsSet class interacts with the Typesense API to manage operations on a
+specific stopwords set. It provides methods to retrieve and delete individual stopwords sets.
+
+For more information regarding Stopwords, refer to the Stopwords [documentation]
+(https://typesense.org/docs/27.0/api/stopwords.html).
+
+This module uses type hinting and is compatible with Python 3.11+ as well as earlier
+versions through the use of the typing_extensions library.
+"""
+
 from typesense.api_call import ApiCall
 from typesense.types.stopword import StopwordDeleteSchema, StopwordsSingleRetrieveSchema
 
 
 class StopwordsSet:
+    """
+    Class for managing individual stopwords sets in Typesense.
+
+    This class provides methods to interact with a specific stopwords set,
+    including retrieving and deleting it.
+
+    Attributes:
+        stopwords_set_id (str): The ID of the stopwords set.
+        api_call (ApiCall): The API call object for making requests.
+    """
+
     def __init__(self, api_call: ApiCall, stopwords_set_id: str) -> None:
+        """
+        Initialize the StopwordsSet object.
+
+        Args:
+            api_call (ApiCall): The API call object for making requests.
+            stopwords_set_id (str): The ID of the stopwords set.
+        """
         self.stopwords_set_id = stopwords_set_id
         self.api_call = api_call
 
-    @property
-    def _endpoint_path(self) -> str:
-        from .stopwords import Stopwords
-
-        return "{0}/{1}".format(Stopwords.RESOURCE_PATH, self.stopwords_set_id)
-
     def retrieve(self) -> StopwordsSingleRetrieveSchema:
+        """
+        Retrieve this specific stopwords set.
+
+        Returns:
+            StopwordsSingleRetrieveSchema: The schema containing the stopwords set details.
+        """
         response: StopwordsSingleRetrieveSchema = self.api_call.get(
-            self._endpoint_path, as_json=True, entity_type=StopwordsSingleRetrieveSchema
+            self._endpoint_path,
+            as_json=True,
+            entity_type=StopwordsSingleRetrieveSchema,
         )
         return response
 
     def delete(self) -> StopwordDeleteSchema:
+        """
+        Delete this specific stopwords set.
+
+        Returns:
+            StopwordDeleteSchema: The schema containing the deletion response.
+        """
         response: StopwordDeleteSchema = self.api_call.delete(
-            self._endpoint_path, entity_type=StopwordDeleteSchema
+            self._endpoint_path,
+            entity_type=StopwordDeleteSchema,
         )
         return response
+
+    @property
+    def _endpoint_path(self) -> str:
+        """
+        Construct the API endpoint path for this specific stopwords set.
+
+        Returns:
+            str: The constructed endpoint path.
+        """
+        from typesense.stopwords import Stopwords
+
+        return "/".join([Stopwords.resource_path, self.stopwords_set_id])
