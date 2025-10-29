972b9a139c83f1d4b1ca2a8c62f4c4b5d32e19b1
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-10-29 13:33:32 +0200

    test(curation_sets): add integration tests for curation sets

--- tests/curation_sets_test.py
+++ tests/curation_sets_test.py
@@ -8,6 +8,7 @@ import requests_mock
 from tests.utils.object_assertions import (
     assert_match_object,
     assert_object_lists_match,
+    assert_to_contain_object,
 )
 from tests.utils.version import is_v30_or_above
 from typesense.api_call import ApiCall
@@ -110,3 +111,65 @@ def test_upsert(fake_curation_sets: CurationSets) -> None:
         assert mock.last_request.json() == payload
 
 
+def test_actual_upsert(
+    actual_curation_sets: CurationSets,
+    delete_all_curation_sets: None,
+) -> None:
+    """Test that the CurationSets object can upsert a curation set on Typesense Server."""
+    response = actual_curation_sets.upsert(
+        "products",
+        {
+            "items": [
+                {
+                    "id": "rule-1",
+                    "rule": {"query": "shoe", "match": "contains"},
+                    "includes": [{"id": "123", "position": 1}],
+                    "excludes": [{"id": "999"}],
+                }
+            ]
+        },
+    )
+
+    assert response == {
+        "items": [
+            {
+                "excludes": [
+                    {
+                        "id": "999",
+                    },
+                ],
+                "filter_curated_hits": False,
+                "id": "rule-1",
+                "includes": [
+                    {
+                        "id": "123",
+                        "position": 1,
+                    },
+                ],
+                "remove_matched_tokens": False,
+                "rule": {
+                    "match": "contains",
+                    "query": "shoe",
+                },
+                "stop_processing": True,
+            },
+        ],
+        "name": "products",
+    }
+
+
+def test_actual_retrieve(
+    actual_curation_sets: CurationSets,
+    delete_all_curation_sets: None,
+    create_curation_set: None,
+) -> None:
+    """Test that the CurationSets object can retrieve curation sets from Typesense Server."""
+    response = actual_curation_sets.retrieve()
+
+    assert isinstance(response, list)
+    assert_to_contain_object(
+        response[0],
+        {
+            "name": "products",
+        },
+    )
