7a0dcc270097cc34fb111cafd74dddb0c1358b7e
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-10-29 13:33:22 +0200

    test(curation_set): add integration tests for curation set

--- tests/curation_set_test.py
+++ tests/curation_set_test.py
@@ -8,6 +8,7 @@ import requests_mock
 from tests.utils.version import is_v30_or_above
 from typesense.client import Client
 from typesense.curation_set import CurationSet
+from typesense.curation_sets import CurationSets
 from typesense.types.curation_set import (
     CurationItemDeleteSchema,
     CurationItemSchema,
@@ -121,3 +122,48 @@ def test_delete_item(fake_curation_set: CurationSet) -> None:
         assert res == json_response
 
 
+def test_actual_retrieve(
+    actual_curation_sets: CurationSets,
+    delete_all_curation_sets: None,
+    create_curation_set: None,
+) -> None:
+    """Test that the CurationSet object can retrieve a curation set from Typesense Server."""
+    response = actual_curation_sets["products"].retrieve()
+
+    assert response == {
+        "items": [
+            {
+                "excludes": [
+                    {
+                        "id": "999",
+                    },
+                ],
+                "filter_curated_hits": False,
+                "id": "rule-1",
+                "includes": [
+                    {
+                        "id": "123",
+                        "position": 1,
+                    },
+                ],
+                "remove_matched_tokens": False,
+                "rule": {
+                    "match": "contains",
+                    "query": "shoe",
+                },
+                "stop_processing": True,
+            },
+        ],
+        "name": "products",
+    }
+
+
+def test_actual_delete(
+    actual_curation_sets: CurationSets,
+    create_curation_set: None,
+) -> None:
+    """Test that the CurationSet object can delete a curation set from Typesense Server."""
+    response = actual_curation_sets["products"].delete()
+
+    print(response)
+    assert response == {"name": "products"}
